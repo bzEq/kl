0374873d55679491aa0561c4f97d1d8f05de3b3a
Kai Luo <gluokai@gmail.com>
2017-01-02 18:10:55 +0800

    add discard logger test

--- logger_test.cc
+++ logger_test.cc
@@ -6,6 +6,9 @@ class L {};
 TEST(L, DefaultLogger) {
   KL_DEBUG("message from %s", "rofl");
   KL_DEBUG_L(kl::logging::Logger::DefaultLogger(), "message from %s", "wtf");
+  auto discard = kl::logging::Logger([](const char *msg) { (void)(msg); });
+  kl::logging::Logger::SetDefaultLogger(std::move(discard));
+  KL_DEBUG("message from %s", "imfao");
 }
 
 int main() {
