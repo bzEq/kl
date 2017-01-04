77c9a764226ac5ad8ebf2ddc1d4c0e194d122730
Kai Luo <gluokai@gmail.com>
2017-01-04 17:15:47 +0800

    add more tests

--- random_test.cc
+++ random_test.cc
@@ -1,12 +1,22 @@
 // Copyright (c) 2017 Kai Luo <gluokai@gmail.com>. All rights reserved.
+#include <algorithm>
+
 #include "random.h"
 #include "testkit.h"
 
 class R {};
 
 TEST(R, UniformSample) {
-  float s = kl::random::UniformSampleFloat();
-  ASSERT(s >= 0 && s <= 1);
+  static const float kEPS = 1e-2;
+  static const int kNumOfSamples = 1 << 16;
+  float sum = 0;
+  for (int i = 0; i < kNumOfSamples; ++i) {
+    float s = kl::random::UniformSampleFloat();
+    ASSERT(s >= 0 && s <= 1);
+    sum += s;
+  }
+  sum /= kNumOfSamples;
+  ASSERT(std::abs(sum - 0.5) <= kEPS);
 }
 
 int main() { return KL_TEST(); }
