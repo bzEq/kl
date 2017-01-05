57756949ff507bf67319b5218f3ac85aa73654cf
Kai Luo <gluokai@gmail.com>
2017-01-05 18:24:35 +0800

    add bind

--- tcp.h
+++ tcp.h
@@ -17,27 +17,43 @@
 namespace kl {
 namespace tcp {
 
-// RETURN: fd
-inline Result<int> Listen(const char *host, uint16_t port) {
+inline Result<int> Socket() {
   int fd = ::socket(AF_INET, SOCK_STREAM, 0);
   if (fd < 0) {
     return kl::Err(errno, std::strerror(errno));
   }
+  return kl::Ok(fd);
+}
+
+inline Result<void> Bind(int fd, const char *host, uint16_t port) {
   struct sockaddr_in addr = {
       .sin_family = AF_INET, .sin_port = htons(port),
   };
   int err = ::inet_aton(host, &addr.sin_addr);
   if (!err) {
-    ::close(fd);
     return kl::Err("invalid ip address: %s", host);
   }
   err = ::bind(fd, reinterpret_cast<struct sockaddr *>(&addr), sizeof(addr));
   if (err != 0) {
-    ::close(fd);
     return kl::Err(errno, "can't bind the socket on port %u, %s", port,
                    std::strerror(errno));
   }
-  err = ::listen(fd, 64);
+  return kl::Ok();
+}
+
+// RETURN: fd
+inline Result<int> Listen(const char *host, uint16_t port) {
+  auto sock = Socket();
+  if (!sock) {
+    return kl::Err(sock.MoveErr());
+  }
+  int fd = *sock;
+  auto bind = Bind(fd, host, port);
+  if (!bind) {
+    ::close(fd);
+    return kl::Err(bind.MoveErr());
+  }
+  int err = ::listen(fd, 64);
   if (err != 0) {
     ::close(fd);
     return kl::Err(errno, "can't listen on port %u, %s", port,
--- tcp_test.cc
+++ tcp_test.cc
@@ -8,8 +8,21 @@ class T {};
 TEST(T, ListenAndSetNonBlocking) {
   auto listen = kl::tcp::Listen("127.0.0.1", 4000);
   ASSERT(listen);
+  kl::env::Defer defer([fd = *listen]() { ::close(fd); });
   ASSERT(kl::env::SetNonBlocking(*listen));
-  ::close(*listen);
+}
+
+TEST(T, Rebind) {
+  auto sock0 = kl::tcp::Socket();
+  ASSERT(sock0);
+  kl::env::Defer defer([fd = *sock0]() { ::close(fd); });
+  auto bind0 = kl::tcp::Bind(*sock0, "127.0.0.1", 4000);
+  ASSERT(bind0);
+  auto sock1 = kl::tcp::Socket();
+  ASSERT(sock1);
+  defer([fd = *sock1]() { ::close(fd); });
+  auto bind1 = kl::tcp::Bind(*sock1, "127.0.0.1", 4000);
+  ASSERT(!bind1);
 }
 
 int main() { return KL_TEST(); }
