be33d2ca3b42059daadc80d23e20080beaa0c1ff
Kai Luo <gluokai@gmail.com>
2017-01-06 16:18:53 +0800

    add udp support

---
+++ udp.h
@@ -0,0 +1,31 @@
+// Copyright (c) 2017 Kai Luo <gluokai@gmail.com>. All rights reserved.
+#ifndef KL_UDP_H_
+#define KL_UDP_H_
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <netinet/udp.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <cerrno>
+#include <cstring>
+
+#include "error.h"
+#include "inet.h"
+
+namespace kl {
+namespace udp {
+
+inline Result<int> Socket() {
+  int fd = ::socket(AF_INET, SOCK_DGRAM, 0);
+  if (fd < 0) {
+    return kl::Err(errno, std::strerror(errno));
+  }
+  return kl::Ok(fd);
+}
+
+}  // udp
+}  // namespace kl
+#endif
---
+++ udp_test.cc
@@ -0,0 +1,35 @@
+// Copyright (c) 2017 Kai Luo <gluokai@gmail.com>. All rights reserved.
+#include <string>
+
+#include "env.h"
+#include "inet.h"
+#include "testkit.h"
+#include "udp.h"
+
+class T {};
+
+TEST(T, Socket) { ASSERT(kl::udp::Socket()); }
+
+TEST(T, SendAndRecv) {
+  const char *host = "127.0.0.1";
+  uint16_t port = 4000;
+  const std::string message("wtf");
+  auto recv_sock = kl::udp::Socket();
+  ASSERT(recv_sock);
+  kl::env::Defer defer([fd = *recv_sock] { ::close(fd); });
+  auto bind = kl::inet::Bind(*recv_sock, host, port);
+  ASSERT(bind);
+  auto send_sock = kl::udp::Socket();
+  ASSERT(send_sock);
+  defer([fd = *send_sock] { ::close(fd); });
+  ASSERT(kl::inet::Sendto(*send_sock, message.c_str(), message.size(), 0, host,
+                          port));
+  char buf[65536];
+  auto recv = kl::inet::RecvFrom(*recv_sock, buf, sizeof(buf), 0);
+  ASSERT(recv);
+  int nread = std::get<0>(*recv);
+  buf[nread] = '\0';
+  ASSERT(std::string(buf) == message);
+}
+
+int main() { return KL_TEST(); }
