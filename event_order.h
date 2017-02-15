1e55985e11411ff23886d653f191b75e2123b89d
Kai Luo <gluokai@gmail.com>
2017-02-15 19:55:11 +0800

    add happened before as a sync method

---
+++ event_order.h
@@ -0,0 +1,57 @@
+// Copyright (c) 2017 Kai Luo <gluokai@gmail.com>. All rights reserved.
+// Use of this source code is governed by the BSD license that can be found in
+// the LICENSE file.
+
+#ifndef KL_EVENT_ORDER_H_
+#define KL_EVENT_ORDER_H_
+#include <condition_variable>
+#include <mutex>
+#include <utility>
+#include <vector>
+
+#include "env.h"
+
+namespace kl {
+namespace event_order {
+
+class Event {
+public:
+  friend void HappenedBefore(Event *a, Event *b);
+  Event() : done_(false) {}
+  Event(const Event &) = delete;
+  Event(Event &&) = delete;
+  void Happened();
+
+private:
+  void Wait();
+  std::mutex mu_;
+  std::condition_variable cv_;
+  bool done_;
+  std::vector<Event *> wait_;
+};
+
+void Event::Wait() {
+  std::unique_lock<std::mutex> l(mu_);
+  while (!done_) {
+    cv_.wait(l);
+  }
+}
+
+void Event::Happened() {
+  assert(!done_);
+  for (auto &event : wait_) {
+    event->Wait();
+  }
+  std::unique_lock<std::mutex> l(mu_);
+  done_ = true;
+  l.unlock();
+  cv_.notify_all();
+}
+
+// TODO(Kai Luo): Cycle detection
+void HappenedBefore(Event *a, Event *b) { b->wait_.push_back(a); }
+
+}  // namespace event_order
+}  // namespace kl
+
+#endif  // KL_EVENT_ORDER_H_
--- filelock_test.cc
+++ filelock_test.cc
@@ -4,35 +4,35 @@
 #include <thread>
 
 #include "env.h"
+#include "event_order.h"
 #include "filelock.h"
 #include "testkit.h"
-#include "wait_group.h"
 
 class T {};
 
 TEST(T, Concurrency) {
-  kl::WaitGroup sync, sync1;
   auto temp = kl::env::MakeTempFile("filelock");
   ASSERT(temp);
   kl::env::Defer defer([fname = *temp] { kl::env::DeleteFile(fname.c_str()); });
-  sync.Add();
-  sync1.Add();
-  std::thread([ fname = *temp, &sync, &sync1 ] {
+  kl::event_order::Event locked0, trylock1, before_unlock0, trylock1_done;
+  kl::event_order::HappenedBefore(&locked0, &trylock1);
+  kl::event_order::HappenedBefore(&trylock1_done, &before_unlock0);
+  std::thread([ fname = *temp, &locked0, &before_unlock0 ] {
     int fd = ::open(fname.c_str(), O_RDONLY);
     ASSERT(fd >= 0);
     kl::env::Defer defer([fd] { ::close(fd); });
     kl::filelock::Lock lock(fd);
     ASSERT(lock.TryLock());
-    sync.Done();
-    sync1.Wait();
+    locked0.Happened();
+    before_unlock0.Happened();
   }).detach();
   int fd = ::open(temp->c_str(), O_RDONLY);
   ASSERT(fd >= 0);
   defer([fd] { ::close(fd); });
   kl::filelock::Lock lock(fd);
-  sync.Wait();
+  trylock1.Happened();
   ASSERT(!lock.TryLock(false, true));
-  sync1.Done();
+  trylock1_done.Happened();
 }
 
 int main() { return KL_TEST(); }
