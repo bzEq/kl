55af2767a15e9f47d7eb9507b680a7a6387b0eb3
Kai Luo <gluokai@gmail.com>
2017-02-15 21:46:27 +0800

    update TODO

--- event_order.h
+++ event_order.h
@@ -49,7 +49,7 @@ inline void Event::Happened() {
   cv_.notify_all();
 }
 
-// TODO(Kai Luo): Cycle detection
+// TODO(Kai Luo): Compile time cycle detection
 inline void HappenedBefore(Event *a, Event *b) { b->wait_.push_back(a); }
 
 }  // namespace event_order
