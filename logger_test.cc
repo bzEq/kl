a3757311f6b1d2911f949ba225511faa701ee1df
Kai Luo <gluokai@gmail.com>
2017-03-16 22:15:22 +0800

    change signature of logger's output function

--- logger_test.cc
+++ logger_test.cc
@@ -9,7 +9,8 @@ class L {};
 TEST(L, DefaultLogger) {
   KL_DEBUG("message from %s", "rofl");
   KL_DEBUG_L(kl::logging::Logger::DefaultLogger(), "message from %s", "wtf");
-  auto discard = kl::logging::Logger([](const char *msg) { (void)(msg); });
+  auto discard =
+      kl::logging::Logger([](const std::string &msg) { (void)(msg); });
   KL_DEBUG_L(discard, "message from %s", "imfao");
   kl::logging::Logger::SetDefaultLogger(std::move(discard));
   KL_DEBUG("message from %s", "imfao");
